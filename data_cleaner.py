c400f08812f5f34255e065d1e585f31d006b7cda
js <50718474+jarell-santella@users.noreply.github.com>
2022-04-08 15:25:07 -0700

    updated basic data cleaning tools

--- data_cleaner.py
+++ data_cleaner.py
@@ -18,17 +18,37 @@ dir_regex = '({})|({})|({})|({})|({})'.format(daisy_regex,dandelion_regex,rose_r
 #dir_regex = '(validation)|(test)'
 
 # Lists where directories will be stored of respective flowers
-daisies, dandelions, roses, sunflowers, tulips = ([] for i in range(5))
+daisy_dir, dandelion_dir, rose_dir, sunflower_dir, tulip_dir = ([] for i in range(5))
+flower_dict = {1: daisy_dir, 2: dandelion_dir, 3: rose_dir, 4: sunflower_dir, 5: tulip_dir}
 
 # Partition string into 3 parts by regex
 def partition_by_regex(regex_search, string):
     partitions = string.partition(regex_search.group(0))
     return (partitions[0], partitions[1], partitions[2])
 
+# Find the first group in regex that matched
+def get_regex_group(pattern, string):
+    n = re.compile(pattern, re.I).groups
+    for i in range(n):
+        if re.search(pattern, string, re.I).group(i+1):
+            return i+1
+    return None
+
+#for root, dirs, files in os.walk('./Datasets'):
+#    regex_search = re.search(dir_regex, root, re.I)
+#    print(root)
+#    if regex_search:
+#        root_components = partition_by_regex(regex_search, root)
+#        n = get_regex_group(dir_regex, root_components[1])
+#        flower_dict.get(n).append(os.path.join(root_components[0],root_components[1]))
+#
+#        print(os.path.join(root_components[0],root_components[1]))
+#        print(root_components[1] + str(get_regex_group(dir_regex, root_components[1])))
+
 # Look in entire project directory for folders of the 5 types of flowers
-for root, dir, files in os.walk('./'):
-    regex_search = re.search(dir_regex, root, re.I)
-    if regex_search:
-        root_components = partition_by_regex(regex_search, root)
-        print(os.path.join(root_components[0],root_components[1]))
-        
+for root, dirs, files in os.walk('./Datasets'):
+    for dir in dirs:
+        regex_search = re.search(dir_regex, dir, re.I)
+        if regex_search:
+            n = get_regex_group(dir_regex, dir)
+            flower_dict.get(n).append(os.path.join(root,dir))
