ff5f838045f556dde75916956e21ac2b3b37a36a
js <50718474+J-Santella@users.noreply.github.com>
2022-05-16 13:51:36 -0700

    updated regex functions

--- regex_functions.py
+++ regex_functions.py
@@ -2,20 +2,23 @@ import re
 
 # Partition string into 3 parts by regex
 def partition_by_regex(pattern, string):
-    match = re.search(pattern, string)
-    if match is not None:
+    match = re.search(pattern, string, re.I)
+    if match:
         partitions = string.partition(match.group(0))
         return partitions[0], partitions[1], partitions[2]
     else:
         return string, '', ''
 
 # Find the first group in regex that matched
-def get_regex_group(pattern, string):
-    n = re.compile(pattern, re.I).groups
-    for i in range(n):
-        try:
-            if re.search(pattern, string, re.I).group(i+1):
-                return i+1
-        except:
-            return None
-    return None
+def get_regex_group(pattern, string, num=True):
+    match = re.search(pattern, string, re.I)
+    if match:
+        n = re.compile(pattern, re.I).groups
+        for i in range(n):
+            if match.group(i+1):
+                if num:
+                    return i+1
+                else:
+                    return match.group(0)
+    else:
+        return None
